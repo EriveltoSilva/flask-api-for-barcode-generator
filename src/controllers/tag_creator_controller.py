693be7bb8b9da7cfc5e20066dfca308cd6546803
otlevire <eriveltoclenio@gmail.com>
2024-02-06 06:27:54 -0800

    refactor: code arquitecture

---
+++ src/controllers/tag_creator_controller.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from src.drivers.barcode_handler import BarCodeHandler
+
+class TagCreatorController:
+    '''
+        Implementing business rules
+    '''
+    def create(self, product_code:str) -> Dict:
+        tag_path = self.__create_tag(product_code=product_code)
+        formatted_response = self.__format_response(tag_path)
+        return formatted_response
+    
+    def __create_tag(self, product_code:str) -> str:
+        barcode_handler = BarCodeHandler()
+        tag_path = barcode_handler.create_barcode(product_code=product_code)
+        return tag_path
+      
+    def __format_response(self, tag_path:str) -> Dict:
+        return {
+            "data":{
+                "type":"Tag Image",
+                "Count":1,
+                "path": f"{tag_path}.png"
+                }
+        }
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,15 +1,20 @@
 from src.views.http_types.http_resquest import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
         Interate with http
     '''
     def validate_and_create(self, http_request:HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
+        response = tag_creator_controller.create(product_code)
 
-        #logica de regra de negocio
+        #business logic
         print("In the view")
-        #retorn http
-        return HttpResponse(status_code=200, body={"status":"very succeful"})
+
+        
+        return HttpResponse(status_code=200, body=response)
